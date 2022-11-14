e4781cd49545387db2ea2170045e679e75b96643
kaoutar boudouft <kaoutarboudouft2@gmail.com>
2022-11-14 22:43:30 +0100

    sending email and emailadresse confirmation

---
+++ src/main/java/com/example/firststringproject/Email/EmailSender.java
@@ -0,0 +1,5 @@
+package com.example.firststringproject.Email;
+
+public interface EmailSender {
+    void send(String to,String email);
+}
--- src/main/java/com/example/firststringproject/Registration/RegistrationService.java
+++ src/main/java/com/example/firststringproject/Registration/RegistrationService.java
@@ -1,6 +1,8 @@
 package com.example.firststringproject.Registration;
 
 
+import com.example.firststringproject.Email.EmailSender;
+import com.example.firststringproject.Email.EmailService;
 import com.example.firststringproject.Registration.Token.ConfirmationToken;
 import com.example.firststringproject.Registration.Token.ConfirmationTokenRepository;
 import com.example.firststringproject.Registration.Token.ConfirmationTokenService;
@@ -22,12 +24,18 @@ public class RegistrationService {
     private final ConfirmationTokenService confirmationTokenService;
     private final ConfirmationTokenRepository confirmationTokenRepository;
 
+    private final EmailService emailService;
     public String register(RegistrationRequest request) {
         Boolean isValidEmail=emailValidator.test(request.getEmail());
         if(!isValidEmail){
             throw new IllegalStateException("email not valid");
         }
-        return studentService.signUpStudent(new Student(request.getName(),request.getEmail(),request.getPassword(), AppStudentRole.Normal));
+        String token= studentService.signUpStudent(new Student(request.getName(),request.getEmail(),request.getPassword(), AppStudentRole.Normal));
+
+        String link="http://localhost:8080/api/v1/registration/confirm?token="+token;
+        emailService.send(request.getEmail(),"<a href='"+link+"'>click to confirm</a>");
+
+        return token;
     }
 
     @Transactional
